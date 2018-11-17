f4fad1611d8f3dc146c9ed4d59ea0d3823c76fdd
Darius Bakunas-Milanowski <bakunas@gmail.com>
2018-11-17 16:22:22 -0500

    minor refactor

--- src/db/models/country.js
+++ src/db/models/country.js
@@ -1,31 +1,28 @@
-module.exports = (sequelize, DataTypes) => {
-  const Country = sequelize.define('Country', {
-    iso: {
-      allowNull: false,
-      type: DataTypes.STRING(2),
-    },
-    name: {
-      allowNull: false,
-      type: DataTypes.STRING(80),
-    },
-    niceName: {
-      allowNull: false,
-      type: DataTypes.STRING(80),
-    },
-    iso3: {
-      default: DataTypes.NULL,
-      type: DataTypes.STRING(3),
-    },
-    numCode: {
-      default: DataTypes.NULL,
-      type: DataTypes.INTEGER.UNSIGNED,
-    },
-    phoneCode: {
-      allowNull: false,
-      type: DataTypes.INTEGER.UNSIGNED,
-    },
-  }, {
-    timestamps: false,
-  });
-  return Country;
-};
+export default (sequelize, DataTypes) => sequelize.define('Country', {
+  iso: {
+    allowNull: false,
+    type: DataTypes.STRING(2),
+  },
+  name: {
+    allowNull: false,
+    type: DataTypes.STRING(80),
+  },
+  niceName: {
+    allowNull: false,
+    type: DataTypes.STRING(80),
+  },
+  iso3: {
+    default: DataTypes.NULL,
+    type: DataTypes.STRING(3),
+  },
+  numCode: {
+    default: DataTypes.NULL,
+    type: DataTypes.INTEGER.UNSIGNED,
+  },
+  phoneCode: {
+    allowNull: false,
+    type: DataTypes.INTEGER.UNSIGNED,
+  },
+}, {
+  timestamps: false,
+});
