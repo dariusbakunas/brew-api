d7777044718007644c838aa199ad3aef8e7af904
Darius Bakunas-Milanowski <bakunas@gmail.com>
2018-11-21 11:45:17 -0500

    update country model

--- src/db/migrations/20181117170407-create-country.js
+++ src/db/migrations/20181117170407-create-country.js
@@ -6,29 +6,14 @@ module.exports = {
       primaryKey: true,
       type: Sequelize.INTEGER,
     },
-    iso: {
+    code: {
       allowNull: false,
       type: Sequelize.STRING(2),
+      unique: true,
     },
     name: {
       allowNull: false,
       type: Sequelize.STRING(80),
-    },
-    niceName: {
-      allowNull: false,
-      type: Sequelize.STRING(80),
-    },
-    iso3: {
-      default: Sequelize.NULL,
-      type: Sequelize.STRING(3),
-    },
-    numCode: {
-      default: Sequelize.NULL,
-      type: Sequelize.INTEGER.UNSIGNED,
-    },
-    phoneCode: {
-      allowNull: false,
-      type: Sequelize.INTEGER.UNSIGNED,
     },
   }),
   down: queryInterface => queryInterface.dropTable('countries'),
--- src/db/models/country.js
+++ src/db/models/country.js
@@ -1,27 +1,11 @@
 export default (sequelize, DataTypes) => sequelize.define('Country', {
-  iso: {
+  code: {
     allowNull: false,
     type: DataTypes.STRING(2),
   },
   name: {
     allowNull: false,
     type: DataTypes.STRING(80),
-  },
-  niceName: {
-    allowNull: false,
-    type: DataTypes.STRING(80),
-  },
-  iso3: {
-    default: DataTypes.NULL,
-    type: DataTypes.STRING(3),
-  },
-  numCode: {
-    default: DataTypes.NULL,
-    type: DataTypes.INTEGER.UNSIGNED,
-  },
-  phoneCode: {
-    allowNull: false,
-    type: DataTypes.INTEGER.UNSIGNED,
   },
 }, {
   timestamps: false,
