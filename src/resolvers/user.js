76d95c5cb36977220548fa8d41c217dce78c6a79
Darius Bakunas-Milanowski <bakunas@gmail.com>
2018-12-22 18:02:24 -0500

    add missing moment dependency

--- src/resolvers/user.js
+++ src/resolvers/user.js
@@ -1,8 +1,8 @@
 import Sequelize from 'sequelize';
 import uuidv4 from 'uuid/v4';
 import moment from 'moment';
-import logger from '../logger';
 import { AuthenticationError, UserInputError } from 'apollo-server-express';
+import logger from '../logger';
 import getUserScopes from '../permissions/getUserScopes';
 import handleError from './handleError';
 
