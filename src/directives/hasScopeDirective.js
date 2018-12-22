0adc464107e273925e6ad24460c9184b4274a61d
Darius Bakunas-Milanowski <bakunas@gmail.com>
2018-12-22 15:48:31 -0500

    handle undefined scopes

--- src/directives/hasScopeDirective.js
+++ src/directives/hasScopeDirective.js
@@ -4,17 +4,16 @@ class HasScopeDirective extends SchemaDirectiveVisitor {
   visitFieldDefinition(field) {
     const { resolve } = field;
     const { scope: expectedScopes } = this.args;
-    field.resolve = async function (...args) {
+    field.resolve = async (...args) => {
       const [, , context] = args;
       if (context.user) {
-        const { user } = context;
+        const { user: { scopes = [] } } = context;
 
-        if (expectedScopes.some(scope => user.scopes.indexOf(scope) !== -1)) {
-          const result = await resolve.apply(this, args);
-          return result;
-        } else {
-          throw new ForbiddenError(`You are not authorized. Expected scopes: ${expectedScopes.join(', ')}`);
+        if (expectedScopes.some(scope => scopes.indexOf(scope) !== -1)) {
+          return resolve.apply(this, args);
         }
+
+        throw new ForbiddenError(`You are not authorized. Expected scopes: ${expectedScopes.join(', ')}`);
       } else {
         throw new AuthenticationError(
           'You must be signed in to view this resource.',
