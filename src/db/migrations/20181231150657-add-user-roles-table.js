8bc24e6cc37e0995118355066ba1d59e625ded83
Darius Bakunas-Milanowski <bakunas@gmail.com>
2018-12-31 11:13:32 -0500

    add roles tables

---
+++ src/db/migrations/20181231150657-add-user-roles-table.js
@@ -0,0 +1,25 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('user_roles', {
+    roleId: {
+      allowNull: false,
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'roles',
+        key: 'id',
+      },
+    },
+    userId: {
+      allowNull: false,
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'users',
+        key: 'id',
+      },
+    },
+  }).then(() => queryInterface.addConstraint('user_roles', ['roleId', 'userId'], {
+    type: 'unique',
+    name: 'user_roles_unique_constraint',
+  })),
+
+  down: queryInterface => queryInterface.dropTable('user_roles'),
+};
