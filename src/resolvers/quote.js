692f69997e0659a2e659dcf48381960f1d2af6e7
Darius Bakunas-Milanowski <bakunas@gmail.com>
2019-02-02 18:19:49 -0500

    support postgresql too

--- src/resolvers/quote.js
+++ src/resolvers/quote.js
@@ -1,16 +1,17 @@
 const Sequelize = require('sequelize');
 
+const env = process.env.NODE_ENV || 'development';
+const config = require('../config/database.js')[env];
+
 const { fn } = Sequelize;
 
 const resolvers = {
   Query: {
-    randomQuote: async (_source, _args, { dataSources }) => {
-      return dataSources.db.Quote.find({
-        order: [
-          fn('rand'),
-        ],
-      });
-    },
+    randomQuote: async (_source, _args, { dataSources }) => dataSources.db.Quote.find({
+      order: [
+        fn(config.dialect === 'mysql' ? 'rand' : 'random'),
+      ],
+    }),
   },
 };
 
